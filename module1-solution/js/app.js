505e080885990d6c6b7f959969fe2e92e3d53f99
Bianca <ing_biank@hotmail.com>
2017-05-25 19:29:23 +0200

    Adds controller to evaluate the input

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -1,4 +1,17 @@
-(function() {
-  'use strict';
-  
+(function(){
+  'use strict'
+
+  angular.module('LunchCheck', [])
+		.controller('lunchCheckController', LunchCheckController)
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.addLunchItems = function() {
+      if ($scope.menu) { 
+        if($scope.menu.split(',').length <= 3) {
+          $scope.message = "Enjoy!";
+        } else {$scope.message = "Too much!";}
+      } else {$scope.message = "Please enter data first";}
+    }
+  }
 })();
