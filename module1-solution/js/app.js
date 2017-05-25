6284b10ae80af3914544090a2e06ccd4567ea1f2
Bianca <ing_biank@hotmail.com>
2017-05-25 19:44:35 +0200

    Adds bootstrap format to notice message

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -2,7 +2,7 @@
   'use strict'
 
   angular.module('LunchCheck', [])
-		.controller('lunchCheckController', LunchCheckController)
+  .controller('lunchCheckController', LunchCheckController)
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
@@ -10,8 +10,12 @@
       if ($scope.menu) { 
         if($scope.menu.split(',').length <= 3) {
           $scope.message = "Enjoy!";
+          $scope.notice = "success";
         } else {$scope.message = "Too much!";}
-      } else {$scope.message = "Please enter data first";}
+      } else {
+        $scope.message = "Please enter data first";
+        $scope.notice = "danger";
+      }
     }
   }
 })();
